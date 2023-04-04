c50334f73167d04768f4b3221498d59975e741f7
roncotten <rcotten@rons-mac-pro.lan>
2023-04-04 14:13:57 -0600

    update

--- cdk/cdk_application.py
+++ cdk/cdk_application.py
@@ -10,12 +10,12 @@ from aws_cdk import (
 
 class CdkApplication(cdk.Stage):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
-        super().__init__(scope, construct_id, **kwargs)
+        super().__init__(scope, construct_id, topic=kwargs.get("topic", **kwargs))
 
-        app_stack = CdkApplicationStack(self, "CdkApplicationStack")
+        app_stack = CdkApplicationStack(self, "CdkApplicationStack", topic)
 
 class CdkApplicationStack(Stack):
-    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+    def __init__(self, scope: Construct, construct_id: str, topic=kwargs.get("topic"), **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
        
-        topic = sns.Topic(self, 'sns-topic')
+        topic = sns.Topic(self, topic)
