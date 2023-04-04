5d719d12405a17dc91df3a5b5c9a76c943bc0ec6
roncotten <rcotten@rons-mac-pro.lan>
2023-04-04 12:20:41 -0600

    update

--- cdk/cdk_pipeline.py
+++ cdk/cdk_pipeline.py
@@ -29,7 +29,8 @@ class CdkPipeline(cdk.Stack):
               env=cdk.Environment(
                  account=f"{AWS_ACCOUNT}",
                  region=f"{AWS_REGION}" 
-              )
+              ),
+              {'sns_topic_name': 'my-topic'}
           )
         )
 
