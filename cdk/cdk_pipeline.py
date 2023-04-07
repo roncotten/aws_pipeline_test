d2b03a84d9ace37eb916f1d384a80e5adea3cf0e
roncotten <rcotten@Rons-Mac-Pro.local>
2023-04-07 17:52:47 -0600

    update

--- cdk/cdk_pipeline.py
+++ cdk/cdk_pipeline.py
@@ -63,7 +63,7 @@ class CdkPipeline(cdk.Stack):
             service_name = deployment+"-service",
             #task_definition=alb_task_definition,
             task_image_options= {
-                "image": ecs_image
+                "image": ecs_image,
                 "container_name": deployment+"-app",
                 "container_port": 80,
                 "execution_role": ecs_role,
