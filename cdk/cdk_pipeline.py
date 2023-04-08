032a5f1a3dbe4143cd2df33789a663ac9a391eeb
roncotten <rcotten@Rons-Mac-Pro.local>
2023-04-07 19:17:54 -0600

    update

--- cdk/cdk_pipeline.py
+++ cdk/cdk_pipeline.py
@@ -96,16 +96,16 @@ class CdkPipeline(cdk.Stack):
                             "$(aws ecr get-login --region $AWS_REGION --no-include-email)",
                             "cd build",
                             "docker build -t $REPOSITORY_URI:latest .",
-                            #"docker tag $REPOSITORY_URI:latest $REPOSITORY_URI:$CODEBUILD_RESOLVED_SOURCE_VERSION"
+                            "docker tag $REPOSITORY_URI:latest $REPOSITORY_URI:$CODEBUILD_RESOLVED_SOURCE_VERSION"
                         ]
                     },
                     "post_build": {
                         "commands": [
                             "docker push $REPOSITORY_URI:latest",
-                            #"docker push $REPOSITORY_URI:$CODEBUILD_RESOLVED_SOURCE_VERSION",
+                            "docker push $REPOSITORY_URI:$CODEBUILD_RESOLVED_SOURCE_VERSION",
                             "export imageTag=$CODEBUILD_RESOLVED_SOURCE_VERSION",
-                            "printf '[{\"name\":\"app\",\"imageUri\":\"%s\"}]' $REPOSITORY_URI:$imageTag > imagedefinitions.json",
-                            "cat imagedefintions.json"
+                            "printf '[{\"name\":\"app\",\"imageUri\":\"%s\"}]' $REPOSITORY_URI:latest > imagedefinitions.json"
+                            #"printf '[{\"name\":\"app\",\"imageUri\":\"%s\"}]' $REPOSITORY_URI:$imageTag > imagedefinitions.json"
                         ]
                     }
                 },
