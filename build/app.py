5140ea20bdf24ebac98c64953450abc1a35dd0bf
roncotten <rcotten@Rons-Mac-Pro.local>
2023-04-10 17:19:33 -0600

    update

--- build/app.py
+++ build/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 @app.route("/")
 
 def main():
-    return "Hello from ECS!!"
+    return "Hello from ECS!!##"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
