90e0f679ee929552f7efdf1fb6faa66fa989db32
roncotten <rcotten@Rons-Mac-Pro.local>
2023-04-10 16:04:32 -0600

    update

--- cdk/cdk_pipeline.py
+++ cdk/cdk_pipeline.py
@@ -21,17 +21,13 @@ source_arn = 'arn:aws:codestar-connections:us-east-1:694795848632:connection/81e
 ecr_repo_name = 'ecosphere'
 vpc_id='vpc-0c3094b44d23a611a'
 
-
 class CdkPipeline(cdk.Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        #########################################################################################################
-        # stack resources 
-        #########################################################################################################
-
         # get context variables
+        init = self.node.try_get_context('init')
         common = self.node.try_get_context('COMMON')
         aws_account = common.get('AWS_ACCOUNT')
         aws_region = common.get('AWS_REGION')
@@ -41,109 +37,100 @@ class CdkPipeline(cdk.Stack):
         environment = environments.get('dev')
         environment_label = environment.get('label')
         stack = environment.get('stack')
-        init = environment.get('init')
         deployment = client + '-' + application + '-' + environment_label + '-' + stack
 
         # create ecr repo
-        ecr_repo = ecr.Repository(self, deployment+"-ecr", repository_name=deployment, image_scan_on_push=True, removal_policy=cdk.RemovalPolicy.DESTROY)
-
-        # create ecs cluster
-        vpc = ec2.Vpc.from_lookup(self, deployment+"-vpc", vpc_id=vpc_id)
-        ecs_cluster = ecs.Cluster(self, deployment+"-ecs", cluster_name=deployment, container_insights=True, vpc=vpc)
-
-        # create fargate alb service
-        execution_policy = iam.ManagedPolicy.from_aws_managed_policy_name(managed_policy_name = "service-role/AmazonECSTaskExecutionRolePolicy")
-        ecs_role = iam.Role(self, deployment+"-ecs_role",
-                                  assumed_by=iam.ServicePrincipal("ecs-tasks.amazonaws.com"),
-                                  managed_policies=[execution_policy], role_name=deployment+"-ecs_role"
-                                  )
-        ecs_image = ecs.ContainerImage.from_registry("694795848632.dkr.ecr.us-east-1.amazonaws.com/doi-ecosphere-d-1:latest")
-        alb_fargate_service = ecs_patterns.ApplicationLoadBalancedFargateService(
-            self, 
-            deployment+"-ecs_service",
-            cluster=ecs_cluster,
-            service_name = deployment+"-service",
-            #task_definition=alb_task_definition,
-            task_image_options= {
-                "image": ecs_image,
-                "container_name": deployment+"-app",
-                "container_port": 80,
-                "execution_role": ecs_role,
-                "enable_logging": True
-                #"execution_role": ""
-                #"task_role": ""
-            },
-            desired_count = 1,
-            load_balancer_name=deployment,
-            listener_port = 80,
-            assign_public_ip=True
-        )
-        fargate_service = alb_fargate_service.service
-
-
-        #########################################################################################################
-        # codebuild project
-        #########################################################################################################
+        ecr_repo = ecr.Repository(self, deployment+"-ecr", repository_name=deployment, image_scan_on_push=True)
 
         codebuild_project = codebuild.PipelineProject(self, "Build", project_name=deployment,
-            environment=codebuild.BuildEnvironment(
-                privileged=True
-            ),
-            build_spec=codebuild.BuildSpec.from_object({
-                "version": "0.2",
-                "phases": {
-                    "build": {
-                        "commands": [
-                            "$(aws ecr get-login --region $AWS_REGION --no-include-email)",
-                            "cd build",
-                            "docker build -t $REPOSITORY_URI:latest .",
-                            "docker tag $REPOSITORY_URI:latest $REPOSITORY_URI:$CODEBUILD_RESOLVED_SOURCE_VERSION"
-                        ]
-                    },
-                    "post_build": {
-                        "commands": [
-                            "cd ..",
-                            "pwd",
-                            "ls -lsFR *",
-                            "docker push $REPOSITORY_URI:latest",
-                            "docker push $REPOSITORY_URI:$CODEBUILD_RESOLVED_SOURCE_VERSION",
-                            "export imageTag=$CODEBUILD_RESOLVED_SOURCE_VERSION",
-                            "printf '[{\"name\":\"app\",\"imageUri\":\"%s\"}]' $REPOSITORY_URI:$imageTag > imagedefinitions.json"
-                        ]
-                    }
-                },
-                "env": {
-                     "exported-variables": ["imageTag"]
-                },
-                "artifacts": {
-                    "files": "imagedefinitions.json",
-                    "secondary-artifacts": {
-                        "imagedefinitions": {
-                            "files": "imagedefinitions.json",
-                            "name": "imagedefinitions"
-                        }
-                    }
-                }
-              }),
-              environment_variables={
-                "AWS_REGION": codebuild.BuildEnvironmentVariable(
-                    value=aws_region
-                ),
-                "REPOSITORY_URI": codebuild.BuildEnvironmentVariable(
-                    value=ecr_repo.repository_uri
-                )
+          environment=codebuild.BuildEnvironment(
+              privileged=True
+          ),
+          build_spec=codebuild.BuildSpec.from_object({
+              "version": "0.2",
+              "phases": {
+                  "build": {
+                      "commands": [
+                          "$(aws ecr get-login --region $AWS_REGION --no-include-email)",
+                          "cd build",
+                          "docker build -t $REPOSITORY_URI:latest .",
+                          "docker tag $REPOSITORY_URI:latest $REPOSITORY_URI:$CODEBUILD_RESOLVED_SOURCE_VERSION"
+                      ]
+                  },
+                  "post_build": {
+                      "commands": [
+                          "cd ..",
+                          "pwd",
+                          "ls -lsFR *",
+                          "docker push $REPOSITORY_URI:latest",
+                          "docker push $REPOSITORY_URI:$CODEBUILD_RESOLVED_SOURCE_VERSION",
+                          "export imageTag=$CODEBUILD_RESOLVED_SOURCE_VERSION",
+                          "printf '[{\"name\":\"app\",\"imageUri\":\"%s\"}]' $REPOSITORY_URI:$imageTag > imagedefinitions.json"
+                      ]
+                  }
+              },
+              "env": {
+                   "exported-variables": ["imageTag"]
+              },
+              "artifacts": {
+                  "files": "imagedefinitions.json",
+                  "secondary-artifacts": {
+                      "imagedefinitions": {
+                          "files": "imagedefinitions.json",
+                          "name": "imagedefinitions"
+                      }
+                  }
               }
+          }),
+            environment_variables={
+              "AWS_REGION": codebuild.BuildEnvironmentVariable(
+                  value=aws_region
+              ),
+              "REPOSITORY_URI": codebuild.BuildEnvironmentVariable(
+                  value=ecr_repo.repository_uri
+              )
+            }
         )
 
         # grant docker push to ecr_repo
         ecr_repo.grant_pull_push(codebuild_project)
 
+        '''
+        # create ecs cluster
+        vpc = ec2.Vpc.from_lookup(self, deployment+"-vpc", vpc_id=vpc_id)
+        ecs_cluster = ecs.Cluster(self, deployment+"-ecs", cluster_name=deployment, container_insights=True, vpc=vpc)
+        execution_policy = iam.ManagedPolicy.from_aws_managed_policy_name(managed_policy_name = "service-role/AmazonECSTaskExecutionRolePolicy")
+        ecs_role = iam.Role(self, deployment+"-ecs_role",
+                                assumed_by=iam.ServicePrincipal("ecs-tasks.amazonaws.com"),
+                                managed_policies=[execution_policy], role_name=deployment+"-ecs_role"
+                              )
+        ecs_image = ecs.ContainerImage.from_registry("694795848632.dkr.ecr.us-east-1.amazonaws.com/doi-ecosphere-d-1:latest")
 
-        #########################################################################################################
-        # pipeline
-        #########################################################################################################
+        # create fargate service
+        alb_fargate_service = ecs_patterns.ApplicationLoadBalancedFargateService(
+          self,
+          deployment+"-ecs_service",
+          cluster=ecs_cluster,
+          service_name = deployment+"-service",
+          #task_definition=alb_task_definition,
+          task_image_options= {
+              "image": ecs_image,
+              "container_name": deployment+"-app",
+              "container_port": 80,
+              "execution_role": ecs_role,
+              "enable_logging": True
+              #"execution_role": ""
+              #"task_role": ""
+          },
+          desired_count = 1,
+          load_balancer_name=deployment,
+          listener_port = 80,
+          assign_public_ip=True
+        )
+        fargate_service = alb_fargate_service.service
+        '''
 
-        # source action 
+        # source action
         source_output = codepipeline.Artifact()
         source_action = codepipeline_actions.CodeStarConnectionsSourceAction(
           action_name="Clone",
@@ -165,27 +152,27 @@ class CdkPipeline(cdk.Stack):
         )
 
         # deploy action
-        deploy_action = codepipeline_actions.EcsDeployAction(
-          action_name="Deploy",
-          service=fargate_service,
-          input=codepipeline.Artifact("imagedefinitions")
-        )
+        #deploy_action = codepipeline_actions.EcsDeployAction(
+        #  action_name="Deploy",
+        #  service=fargate_service,
+        #  input=codepipeline.Artifact("imagedefinitions")
+        #)
 
-        # create pipeline 
-        codepipeline.Pipeline(self, deployment+"-pipeline", pipeline_name=deployment,
+        # create pipeline
+        pipeline = codepipeline.Pipeline(self, deployment+"-pipeline", pipeline_name=deployment,
             stages=[
               {
                 "stageName": "source",
                 "actions": [source_action]
-              }, 
+              },
               {
                 "stageName": "build",
                 "actions": [build_action]
-              }, 
-              {
-                "stageName": "deploy",
-                "actions": [deploy_action]
-              }
+              },
+              #{
+              #  "stageName": "deploy",
+              #  "actions": [deploy_action]
+              #}
             ]
-        )
+          )
 
