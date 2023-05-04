ca1289335a73c9443fc7cf004abadb4ca7fbcdb9
roncotten <rcotten@rons-mac-pro.lan>
2023-05-04 14:08:08 -0600

    update

--- build/app.py
+++ build/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 @app.route("/")
 
 def main():
-    return "Hello from ECS hell!"
+    return "Hello from AWS hell!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
